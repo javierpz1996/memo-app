cca3e6a623a9effa476baa5f3dacee7d3beff4a3
javpeniaz96 <javier900dg@hotmail.com>
2025-11-02 21:33:52 -0300

    Corrijo base path para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // 🔥 Importante para que funcione en Vercel
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
